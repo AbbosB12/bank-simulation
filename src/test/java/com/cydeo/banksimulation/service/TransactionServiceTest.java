1b75f7c9e4856837d4703bdc839b91eadc6d61b8
smilecyber <ismail.yildirim@cybertekschool.com>
2022-07-06 03:05:39 +0300

    Update TransactionServiceTest.java

--- src/test/java/com/cydeo/banksimulation/service/TransactionServiceTest.java
+++ src/test/java/com/cydeo/banksimulation/service/TransactionServiceTest.java
@@ -5,6 +5,7 @@ import com.cydeo.banksimulation.entity.Account;
 import com.cydeo.banksimulation.entity.Transaction;
 import com.cydeo.banksimulation.enums.AccountStatus;
 import com.cydeo.banksimulation.enums.AccountType;
+import com.cydeo.banksimulation.exception.AccountNotVerifiedException;
 import com.cydeo.banksimulation.exception.BadRequestException;
 import com.cydeo.banksimulation.mapper.TransactionMapper;
 import com.cydeo.banksimulation.repository.TransactionRepository;
@@ -60,6 +61,67 @@ public class TransactionServiceTest {
         assertInstanceOf(BadRequestException.class, throwable);
         BadRequestException badRequestException = (BadRequestException) throwable;
         assertEquals("Sender or receiver can not be null", badRequestException.getMessage());
+    }
+
+    @Test
+    public void should_throw_bad_request_exception_when_receiver_account_is_null(){
+        AccountDTO sender = prepareAccountDTO(5L,new BigDecimal(150),AccountStatus.ACTIVE, true, 125L, AccountType.CHECKINGS);
+        Throwable throwable = catchThrowable(() -> transactionService.makeTransfer(BigDecimal.TEN
+                ,new Date(), sender, null, "message"));
+        assertNotNull(throwable);
+        assertInstanceOf(BadRequestException.class, throwable);
+        BadRequestException badRequestException = (BadRequestException) throwable;
+        assertEquals("Sender or receiver can not be null", badRequestException.getMessage());
+    }
+
+    @Test
+    public void should_throw_bad_request_exception_when_sender_and_receiver_account_id_is_same(){
+        AccountDTO sender = prepareAccountDTO(5L,new BigDecimal(150),AccountStatus.ACTIVE, true, 125L, AccountType.CHECKINGS);
+        AccountDTO receiver = prepareAccountDTO(5L,new BigDecimal(150),AccountStatus.ACTIVE, true, 125L, AccountType.CHECKINGS);
+        Throwable throwable = catchThrowable(() -> transactionService.makeTransfer(BigDecimal.TEN
+                ,new Date(), sender, receiver, "message"));
+        assertNotNull(throwable);
+        assertInstanceOf(BadRequestException.class, throwable);
+        BadRequestException badRequestException = (BadRequestException) throwable;
+        assertEquals("Sender account needs to be different from recaiver account", badRequestException.getMessage());
+    }
+
+    @Test
+    public void should_throw_bad_request_exception_when_sender_account_status_is_deleted(){
+        AccountDTO sender = prepareAccountDTO(2L,new BigDecimal(150),AccountStatus.DELETED, true, 125L, AccountType.CHECKINGS);
+        AccountDTO receiver = prepareAccountDTO(5L,new BigDecimal(150),AccountStatus.ACTIVE, true, 125L, AccountType.CHECKINGS);
+        Throwable throwable = catchThrowable(() -> transactionService.makeTransfer(BigDecimal.TEN
+                ,new Date(), sender, receiver, "message"));
+        assertNotNull(throwable);
+        assertInstanceOf(BadRequestException.class, throwable);
+        BadRequestException badRequestException = (BadRequestException) throwable;
+        assertEquals("Sender account is deleted, you can not send money from this account", badRequestException.getMessage());
+    }
+
+    @Test
+    public void should_throw_bad_request_exception_when_receiver_account_status_is_deleted(){
+        AccountDTO sender = prepareAccountDTO(2L,new BigDecimal(150),AccountStatus.ACTIVE, true, 125L, AccountType.CHECKINGS);
+        AccountDTO receiver = prepareAccountDTO(5L,new BigDecimal(150),AccountStatus.DELETED, true, 125L, AccountType.CHECKINGS);
+        Throwable throwable = catchThrowable(() -> transactionService.makeTransfer(BigDecimal.TEN
+                ,new Date(), sender, receiver, "message"));
+        assertNotNull(throwable);
+        assertInstanceOf(BadRequestException.class, throwable);
+        BadRequestException badRequestException = (BadRequestException) throwable;
+        assertEquals("Receiver account is deleted, you can not send money to this account", badRequestException.getMessage());
+    }
+
+    @Test
+    public void should_throw_account_not_verified_exception_sender_account_is_not_verified(){
+        AccountDTO sender = prepareAccountDTO(2L,new BigDecimal(150),AccountStatus.ACTIVE, false, 125L, AccountType.CHECKINGS);
+        AccountDTO receiver = prepareAccountDTO(5L,new BigDecimal(150),AccountStatus.ACTIVE, true, 125L, AccountType.CHECKINGS);
+        when(accountService.retrieveById(2L)).thenReturn(sender);
+
+        Throwable throwable = catchThrowable(() -> transactionService.makeTransfer(BigDecimal.TEN
+                ,new Date(), sender, receiver, "message"));
+        assertNotNull(throwable);
+        assertInstanceOf(AccountNotVerifiedException.class, throwable);
+        AccountNotVerifiedException accountNotVerifiedException = (AccountNotVerifiedException) throwable;
+        assertEquals("account not verified yet.", accountNotVerifiedException.getMessage());
     }
 
 
